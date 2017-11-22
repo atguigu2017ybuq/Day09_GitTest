0f59f1154bf3bc346cc07d61b3b6b72ce7d34702
tom0706 <tom0706@atguigu.com>
2017-11-22 15:35:23 +0800

    for conflict

--- src/main/java/com/atguigu/entities/GitEntity.java
+++ src/main/java/com/atguigu/entities/GitEntity.java
@@ -3,7 +3,7 @@ package com.atguigu.entities;
 public class GitEntity {
 	
 	public static void main(String[] args) {
-		System.out.println("edit by kepler...");
+		System.out.println("edit by kepler...[sts for conflict]");
 		System.out.println("edit by sts...");
 	}
 
